4fd81d04ed9c001c99818045effea3fa8fef71c4
wyslexa <privet@alexsyw.me>
2018-07-24 05:52:39 +1100

    add

---
+++ error/index.php
@@ -0,0 +1,8 @@
+<?php 
+$project_name = 'alexsyw error page';
+include 'www/new_head.php';
+
+echo '<h1>Поздравляю! Ты наткнулся на ошибку.</h1>';
+echo '<p>Теперь вернись обратно, а если всё равно ошибка то пиши на bug [@] alexsyw [.] me</p>';
+echo '<p style="text-align: center;"><a href="http://alexsywindows.io">Вернуться на главную</a></p>';
+?>
